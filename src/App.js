77d0fbcc92a3eadbf695f19b1bdc742ff5eab665
Nihal Jadhav <nihal.jadhav@arowanaconsulting.com>
2023-01-01 23:32:40 +0530

    72 Videos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ExpenseItem from "./component/ExpenseItem";
+// import ExpenseItem from "./component/ExpenseItem";
+import Expenses from "./component/Expenses/Expenses";
 
 function App() {
   const expense = [
@@ -31,26 +32,7 @@ function App() {
   return (
     <div>
       <h2>Lets Start</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      ></ExpenseItem>
+      <Expenses items={expense} />
     </div>
   );
 }
